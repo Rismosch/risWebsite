6062dae8c58441ed4fdce5ce5454cf8d0afb34be
Rismosch Desktop <simon.sutoris@googlemail.com>
2021-01-23 11:41:22 +0100

    scaled for mobile and implemented disqus

--- website/article.php
+++ website/article.php
@@ -33,9 +33,28 @@ if($dbSelectConnection){
 				if(file_exists($content))
 					include $content;
 				else
+				{
 					echo "<h1>:(</h1><p>Could not find article with id={$article_id}</p>";
+					$article_id = 0;
+				}
 				
-				echo "\n";
+				echo "
+				<div id=\"disqus_thread\"></div>
+				<script>
+					var disqus_config = function () {
+						this.page.url = \"https://www.rismosch.com/article?id={$article_id}\";
+						this.page.identifier = {$article_id};
+					};
+					
+					(function() {
+						var d = document, s = d.createElement('script');
+						s.src = 'https://rismosch.disqus.com/embed.js';
+						s.setAttribute('data-timestamp', +new Date());
+						(d.head || d.body).appendChild(s);
+					})();
+				</script>
+				<noscript>Please enable JavaScript to view the <a href=\"https://disqus.com/?ref_noscript\">comments powered by Disqus.</a></noscript>
+				";
 			?>
 		</div>
 		
